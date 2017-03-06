1b42a41118f005d1481d3e871660b669c7939567
loglogin2 <i9991559@gmail.com>
2017-03-06 19:05:34 +0300

    Zeros/feature: implement for small numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function zeros(expression) {
   // your solution
+    var arr = expression.split('*');
+    var ans = 1;
+    var tmp = 1;
+    for(var i = 0 ; i < arr.length ; i ++) {
+        if(arr[i].indexOf("!!") >  0 )
+        {
+            arr[i] = parseInt(arr[i],10);
+            var chet = 1;
+            if(arr[i] % 2 == 0)
+                chet = 1;
+            else
+                chet = 0;
+            for ( var j = chet ; j < arr[i] ; j ++)
+            {
+                tmp = tmp*(j+1);
+                j++;
+            }
+            arr[i] = tmp ;
+            tmp = 1 ;
+        }
+        else
+        {
+            arr[i] = parseInt(arr[i],10);
+            for(var j = 0 ; j < arr[i] ; j ++)
+                tmp = tmp * (j+1) ;
+            arr[i] = tmp;
+            tmp = 1;
+        }
+        ans = ans * arr[i];
+    }
+    ans = ans.toString();
+    var zeros = 0 ;
+    for ( var i = ans.length - 1  ; i > 0 ; i --)
+    {
+        if ( ans[i] == '0')
+            zeros ++;
+        else
+            return zeros;
+    }
+
 }
